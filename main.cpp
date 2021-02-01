ed42a50efc8876d274dd5225a6322657c015d305
rendyrianda <ridrareday@gmail.com>
2021-02-01 16:44:51 +0700

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "menu.cpp"
+#include "monster.cpp"
+#include "battle.cpp"
+#include "items.cpp"
+#include "quest.cpp"
+#include "store.cpp"
+#include "player.cpp"
+
+using namespace std;
+int main()
+{
+	   LayarAwal();
+	   return 0;
+}
+
